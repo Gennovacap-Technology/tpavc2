5a5fa8098b00617bdf3fc8825bf39fc462605b9e
Rafael Fragoso <rafaelfragosom@gmail.com>
2013-11-24 20:51:02 -0200

    fix the redirect

--- app/controllers/passport_controller.rb
+++ app/controllers/passport_controller.rb
@@ -97,7 +97,7 @@ class PassportController < ApplicationController
 			RenewMailer.renew_mail(@renew).deliver
 			
 			flash[:notice] = "Your form was sent with success!"
-			redirect_to "/#{action}"
+			redirect_to "/passport/#{action}"
 		else
 			render :action => action
 		end
